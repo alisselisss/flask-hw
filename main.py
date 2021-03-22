8c37125b4fdafd38012c12e927b1d9fc2e80174b
alisselisss <74914260+alisselisss@users.noreply.github.com>
2021-03-22 18:06:47 +0500

    все еще не работает(((

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, url_for, request
 from werkzeug.exceptions import abort
 from werkzeug.utils import redirect
@@ -159,6 +161,8 @@ def load_user(user_id):
 def main():
     db_session.global_init("db/blogs.db")
     app.run(host='127.0.0.1', port=8080)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
